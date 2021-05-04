0060ffd1d998e2b5e601872b4f68dac266a98ed0
DESKTOP-7PBU6LV\user <ckalswl331@naver.com>
2021-05-04 21:58:58 +0900

    Adding the Buzzer

--- GuessTheWord/app/src/main/java/com/example/guesstheword/screens/game/GameFragment.kt
+++ GuessTheWord/app/src/main/java/com/example/guesstheword/screens/game/GameFragment.kt
@@ -1,11 +1,15 @@
 package com.example.guesstheword.screens.game
 
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -51,9 +55,31 @@ class GameFragment : Fragment() {
             }
         })
 
+        // Buzzes when triggered with different buzz events
+        viewModel.eventBuzz.observe(viewLifecycleOwner, Observer { buzzType ->
+            if (buzzType != GameViewModel.BuzzType.NO_BUZZ) {
+                buzz(buzzType.pattern)
+                viewModel.onBuzzComplete()
+            }
+        })
+
         return binding.root
 
     }
+
+    private fun buzz(pattern: LongArray) {
+        val buzzer = activity?.getSystemService<Vibrator>()
+
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
+        }
+    }
+
 
     /**
      * onSaveInstanceState 를 이용하는 것도 좋은 방법이지만
