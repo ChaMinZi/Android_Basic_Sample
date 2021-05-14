9a598ba8a00216bedc681fafd2b5defec9b98e1c
DESKTOP-7PBU6LV\user <ckalswl331@naver.com>
2021-05-15 00:10:54 +0900

    Testing the Room Database

--- TrackmySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ TrackmySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -21,9 +21,9 @@ import androidx.room.*
 
 @Dao
 interface SleepDatabaseDao {
+    // 컴파일시간 동안 룸은 코드를 발생시킵니다. Object를 데이터베이스에 row 값으로 삽입할 코드를
     @Insert
     suspend fun insert(night: SleepNight)
-    // 컴파일시간 동안 룸은 코드를 발생시킵니다. Object를 데이터베이스에 row 값으로 삽입할 코드를
 
     @Update
     suspend fun update(night: SleepNight)
@@ -34,11 +34,14 @@ interface SleepDatabaseDao {
     @Query("DELETE FROM daily_sleep_quality_table")
     suspend fun clear()
 
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
     /**
      * 내림차순으로 정렬된 데이터 중에 첫 번째 1개의 값만 리턴해라
      * 리턴값은 nullable하다. 왜냐하면 처음이나 모든 content를 지우고 난 후에는
      * 해당 Table에 저장된 값이 없을 것이기 때문에 null도 반환 가능해야 합니다.
-    **/
+     **/
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
     suspend fun getTonight(): SleepNight?
 }
