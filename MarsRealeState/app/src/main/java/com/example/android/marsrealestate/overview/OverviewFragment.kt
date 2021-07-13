a02a74717703a445ae76aa5f76fb12d5e32b9662
DESKTOP-7PBU6LV\user <ckalswl331@naver.com>
2021-07-13 18:14:39 +0900

    Display Images in a Grid  1. DiffUtil 사용하기  2. DataBinding 넘겨주기  3. Binding Adapter 사용하기

--- MarsRealeState/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ MarsRealeState/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -26,7 +26,7 @@ class OverviewFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = GridViewItemBinding.inflate(inflater)
+        val binding = FragmentOverviewBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
@@ -34,6 +34,7 @@ class OverviewFragment : Fragment() {
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
 
+        binding.photosGrid.adapter = PhotoGridAdapter()
         setHasOptionsMenu(true)
         return binding.root
     }
