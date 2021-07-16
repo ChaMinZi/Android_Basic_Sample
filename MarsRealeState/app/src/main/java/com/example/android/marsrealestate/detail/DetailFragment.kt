676922b8eb055b5ddbb9127614a84b4097ee6fe9
DESKTOP-7PBU6LV\user <ckalswl331@naver.com>
2021-07-16 17:00:50 +0900

    Add Parcelable in MarsProperty and move  the Detail Screen logic * Parcelable 객체 만들기 * RecyclerView에 OnclickListener 추가 * DetailFragment로 이동하는 로직 추가

--- MarsRealeState/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ MarsRealeState/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -19,6 +19,12 @@ class DetailFragment : Fragment() {
         val application = requireNotNull(activity).application
         val binding = FragmentDetailBinding.inflate(inflater)
         binding.lifecycleOwner = this
+
+        // !!은 null일 경우 nullpointexception을 throw 실제 프러덕트에서는 에러처리를 해주는게 좋음
+        val marsProperty = DetailFragmentArgs.fromBundle(arguments!!).selectedProperty
+        val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+
+        binding.viewModel = ViewModelProvider(this, viewModelFactory).get(DetailViewModel::class.java)
         return binding.root
     }
 }
