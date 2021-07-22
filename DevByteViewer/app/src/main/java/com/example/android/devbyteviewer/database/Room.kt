3941cd396b820263b37331fb96fdec94a5bcaee6
DESKTOP-7PBU6LV\user <ckalswl331@naver.com>
2021-07-22 20:40:59 +0900

    Add a DatabaseVideo Entity and  the VideoDao 1. DB에 저장할 DatabaseVideo의 Entity를 작성 2. Database objects를 domain objects로 바꿔줄 converts 확장 함수 구현 3. data transfer objects(네트워킹 결과값)를 database objects로 바꿔줄 converts 확장 함수 구현 4. VideoDao 구현

--- DevByteViewer/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ DevByteViewer/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -1,18 +1,25 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.example.android.devbyteviewer.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("select * from databasevideo")
+    fun getVideos(): List<DatabaseVideo>
+
+    /**
+     * @[vararg] = variable arguments
+     * it's how a function can take an unknown number of arguments in Kotlin.
+     * (Kotlin에서 함수가 알 수 없는 수의 인수를 취할 수 있는 방법)
+     * 여기서 실제로는 array를 전달할 것입니다.
+
+     * @[onConflict]
+     * 동일한 PrimaryKey가 Database에 있는 경우, exception이 발생하기 때문에
+     * OnConflictStrategy 를 설정해줌
+    **/
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
